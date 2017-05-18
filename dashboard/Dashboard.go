63bdb8c06d0f92de7f80138065f8e8aa1a453ecc
Clint Armstrong <clint@clintarmstrong.net>
2017-05-18 13:09:00 -0400

    That's a bad idea. This is for development, just run without security for dev

--- dashboard/Dashboard.go
+++ dashboard/Dashboard.go
@@ -45,7 +45,7 @@ func (d *Dashboard) apiHandler(w http.ResponseWriter, r *http.Request) {
 		case "applications":
 			w.Header().Set("Content-Type", "application/json")
 			w.Header().Set("Content-Encoding", "gzip")
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			//w.Header().Set("Access-Control-Allow-Origin", "*")
 			gz := gzip.NewWriter(w)
 			defer gz.Close()
 			err := json.NewEncoder(gz).Encode(d.applications.GetApplicationStatus())

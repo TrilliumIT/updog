9f7444dea1c80dd7a01d2ec6f0f0e81ff2f9fc8a
Clint Armstrong <clint@clintarmstrong.net>
2017-05-23 15:10:19 -0400

    rewrite bosun updates

--- opentsdb/updates.go
+++ opentsdb/updates.go
@@ -5,23 +5,50 @@ import (
 )
 
 func (c *Client) Subscribe(conf *updog.Config) error {
-	for an, app := range conf.Applications.Applications {
-		ac := c.NewClient(map[string]string{"application": an})
-		for sn, svc := range app.Services {
-			sc := ac.NewClient(map[string]string{"service": sn})
-			go func(svc *updog.Service, sc *Client) {
-				sub := svc.Subscribe(false, 255, 0, false)
-				for ss := range sub.C {
-					sc.Submit("updog.service.degraded", ss.Degraded, ss.TimeStamp)
-					sc.Submit("updog.service.failed", ss.Failed, ss.TimeStamp)
-					for addr, inst := range ss.Instances {
-						ic := sc.NewClient(map[string]string{"instance": addr})
-						ic.Submit("updog.instance.up", inst.Up, inst.TimeStamp)
-						ic.Submit("updog.instance.response_time", inst.ResponseTime, inst.TimeStamp)
+	go func() {
+		sub := conf.Applications.Subscribe(false, 255, 0, false)
+		for ass := range sub.C {
+			ts := ass.TimeStamp
+			c.Submit("updog.applications_total", ass.ApplicationsTotal, ts)
+			c.Submit("updog.applications_up", ass.ApplicationsUp, ts)
+			c.Submit("updog.applications_degraded", ass.ApplicationsDegraded, ts)
+			c.Submit("updog.applications_failed", ass.ApplicationsFailed, ts)
+			c.Submit("updog.services_total", ass.ServicesTotal, ts)
+			c.Submit("updog.services_up", ass.ServicesUp, ts)
+			c.Submit("updog.services_degraded", ass.ServicesDegraded, ts)
+			c.Submit("updog.services_failed", ass.ServicesFailed, ts)
+			c.Submit("updog.instances_total", ass.InstancesTotal, ts)
+			c.Submit("updog.instances_up", ass.InstancesUp, ts)
+			c.Submit("updog.instances_failed", ass.InstancesFailed, ts)
+			for an, a := range ass.Applications {
+				ac := c.NewClient(map[string]string{"application": an})
+				ts := a.TimeStamp
+				ac.Submit("updog.application.failed", a.Failed, ts)
+				ac.Submit("updog.application.degraded", a.Degraded, ts)
+				ac.Submit("updog.application.services_total", a.ServicesTotal, ts)
+				ac.Submit("updog.application.services_up", a.ServicesUp, ts)
+				ac.Submit("updog.application.services_degraded", a.ServicesDegraded, ts)
+				ac.Submit("updog.application.services_failed", a.ServicesFailed, ts)
+				ac.Submit("updog.application.instances_total", a.InstancesTotal, ts)
+				ac.Submit("updog.application.instances_up", a.InstancesUp, ts)
+				ac.Submit("updog.application.instances_failed", a.InstancesFailed, ts)
+				for sn, s := range a.Services {
+					sc := ac.NewClient(map[string]string{"service": sn})
+					ts := s.TimeStamp
+					sc.Submit("updog.service.failed", s.Failed, ts)
+					sc.Submit("updog.service.degraded", s.Degraded, ts)
+					sc.Submit("updog.service.instances_total", s.InstancesTotal, ts)
+					sc.Submit("updog.service.instances_up", s.InstancesUp, ts)
+					sc.Submit("updog.service.instances_failed", s.InstancesFailed, ts)
+					for in, i := range s.Instances {
+						ic := sc.NewClient(map[string]string{"instance": in})
+						ts := i.TimeStamp
+						ic.Submit("updog.instance.up", i.Up, ts)
+						ic.Submit("updog.instance.response_time", i.ResponseTime, ts)
 					}
 				}
-			}(svc, sc)
+			}
 		}
-	}
+	}()
 	return nil
 }

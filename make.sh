a770aa4134169f11a3cdc354d306035346c6cff0
Travis Hegner <travis.hegner@gmail.com>
2018-09-07 09:46:24 -0400

    update depends, add metalinter info

--- make.sh
+++ make.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "Linting..."
+gometalinter --vendor ./...
+
+echo "Generating code..."
 go generate ./dashboard && go generate ./types/subscriber.go
+
+echo "Building..."
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o updog .

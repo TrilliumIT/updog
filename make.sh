f079a2a7b227c354520ab116e13a93a082c7ce0c
Clint Armstrong <clint@clintarmstrong.net>
2018-09-14 15:33:32 -0400

    fail early on lint errors, and increase lint deadline

--- make.sh
+++ make.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 echo "Generating code..."
 go generate ./dashboard && go generate ./types/subscriber.go
 
 echo "Linting..."
-gometalinter --exclude=gen- --exclude=bindata --vendor ./...
+gometalinter --exclude=gen- --exclude=bindata --deadline=300s --vendor ./...
 
 echo "Building..."
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o updog .
